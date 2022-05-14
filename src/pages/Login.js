0c05159d1cb9f54802ec3ff3d7678ce904ff4a52
Priscilla <rabhas.vashisht@gmail.com>
2022-05-14 00:15:28 -0400

    add login page and style for it

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,24 @@
+import Nav from "../components/Nav"
+
 const Login = () => {
+
+  const authToken = false
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
   return (
-    <div>Hey this is Login page</div>
+    <>
+    <Nav/>
+    <div className="login">
+      <h1> Pets-Ville </h1>
+      <button className="primary-button" onClick={handleClick}>
+        {authToken ? 'Signout' : 'Create Account' }
+    </button>  
+
+  </div>
+  </>
   )
 }
 export default Login
