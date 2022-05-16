b4e5879f0a73fa2f8f32bb64aaed667919e4c8d1
mariohamameh <mariohamameh97@gmail.com>
2022-05-16 14:38:32 -0400

    test

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,14 +1,13 @@
 import { useState } from "react";
-import Nav from '../components/Nav';
+import Nav from "../components/Nav";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../Register-Form.css";
 
 const Register = () => {
-   
   const navigate = useNavigate();
   const [data, setData] = useState({
-    Name: "",
+    pet_name: "",
     species: "",
     size: "",
     breed: "",
@@ -19,7 +18,7 @@ const Register = () => {
     event.preventDefault();
     console.log(data);
     axios
-      .post("http://localhost:3001/users", data)
+      .post("http://localhost:3002/profiles", data)
       .then(function (response) {
         console.log(response);
         navigate.push("/Home");
@@ -36,77 +35,75 @@ const Register = () => {
   };
 
   return (
-
     <>
-      <Nav
-        minimal={true}
-        setShowModal={() => {}}
-        showModal={false}
-      />
-    
-      <div className="sign-up-form">
+      <Nav minimal={true} setShowModal={() => {}} showModal={false} />
 
-      <form className="form-box" onSubmit={onSubmit}>
-      <section>
-        Name: 
-        <input
-          id="name"
-          type="text"
-          name="name"
-          placeholder="name"
-          required={true}
-          onChange={onChange}
-          value={data.name}
-        /><br/>
-        Species:
-        <input
-          type="text"
-          name="species"
-          placeholder="species"
-          required={true}
-          onChange={onChange}
-          value={data.species}
-          /><br/>
-          Size:
-        <input
-          type="text"
-          name="size"
-          placeholder="size"
-          required={true}
-          onChange={onChange}
-          value={data.size}
-        /><br/>
-        Breed:
-         <input
-          type="text"
-          name="breed"
-          placeholder="breed"
-          required={true}
-          onChange={onChange}
-          value={data.breed}
-        /><br/>
-        Age:
-        <input
-          id="age"
-          type="number"
-          name="age"
-          placeholder="age"
-          required={true}
-          onChange={onChange}
-          value={data.age}
-        /><br/>
-        <label for="uploads">Choose the images you want to upload:</label>
-        <input
-          type="file"
-          id="uploads"
-          name="image"
-          accept=".jpg, .jpeg, .png, .gif"
-          multiple
-        />
-      
+      <div className="sign-up-form">
+        <form className="form-box" onSubmit={onSubmit}>
+          <section>
+            Name:
+            <input
+              id="pet_name"
+              type="text"
+              name="pet_name"
+              placeholder="Name"
+              required={true}
+              onChange={onChange}
+              value={data.pet_name}
+            />
+            <br />
+            Species:
+            <input
+              type="text"
+              name="species"
+              placeholder="species"
+              required={true}
+              onChange={onChange}
+              value={data.species}
+            />
+            <br />
+            Size:
+            <input
+              type="text"
+              name="size"
+              placeholder="size"
+              required={true}
+              onChange={onChange}
+              value={data.size}
+            />
+            <br />
+            Breed:
+            <input
+              type="text"
+              name="breed"
+              placeholder="breed"
+              required={true}
+              onChange={onChange}
+              value={data.breed}
+            />
+            <br />
+            Age:
+            <input
+              id="age"
+              type="number"
+              name="age"
+              placeholder="age"
+              required={true}
+              onChange={onChange}
+              value={data.age}
+            />
+            <br />
+            <label for="uploads">Choose the images you want to upload:</label>
+            <input
+              type="file"
+              id="uploads"
+              name="image"
+              accept=".jpg, .jpeg, .png, .gif"
+              multiple
+            />
           </section>
-     
-      {/* <div className="sign-up-form__field-wrapper">
+
+          {/* <div className="sign-up-form__field-wrapper">
         Species:
         <input
           type="text"
@@ -161,13 +158,10 @@ const Register = () => {
         />
       </div> */}
 
-      <button className="sign-up-form-button">Sign Up</button>
-      
-      </form>
-    </div>
-    
+          <button className="sign-up-form-button">Sign Up</button>
+        </form>
+      </div>
     </>
-   
   );
 };
 export default Register;
--- server/server.js
+++ server/server.js
@@ -1,41 +1,38 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3002;
-const pool = require("./database")
-const chats = require("./routes/chatRoutes")
-const users = require("./routes/userRoutes")
-const profiles = require("./routes/profileRoutes")
-const conversations = require("./routes/conversationRoutes")
-const locations = require("./routes/locationRoutes")
+const pool = require("./database");
+const chats = require("./routes/chatRoutes");
+const users = require("./routes/userRoutes");
+const profiles = require("./routes/profileRoutes");
+const conversations = require("./routes/conversationRoutes");
+const locations = require("./routes/locationRoutes");
 
 app.use(cors());
 app.use(express.json());
 
 app.listen(3002, () => {
-  console.log("server has started on port 3001")
-})
+  console.log("server has started on port 3002");
+});
 
 //routues//
 
-
-app.get("/", (req,res) => {
-    res.send("Welcome to the the API SERVER")
+app.get("/", (req, res) => {
+  res.send("Welcome to the the API SERVER");
 });
 
 //chat routes
-app.use("/chats", chats)
+app.use("/chats", chats);
 
 //conversation routes
-app.use("/conversations",conversations)
+app.use("/conversations", conversations);
 
 //location routes
-app.use("/locations",locations)
+app.use("/locations", locations);
 
 //profile route
-app.use("/profiles", profiles)
-
+app.use("/profiles", profiles);
 
 //user routes
-app.use("/users", users)
-
+app.use("/users", users);
